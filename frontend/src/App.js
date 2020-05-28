0808c664358318a9aaf91fa412546b4b9809756e
Ivan Babiy <ivanb07545@gmail.com>
2020-05-28 13:08:39 +0300

    add navigation in App: Router, Route, Switch, Redirect, Link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Users from "./pages/user/Users";
+import NewNews from "./pages/news/NewNews";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/users" exact>
+          <Users />
+        </Route>
+        <Route path="/news/new" exact>
+          <NewNews />
+        </Route>
+        <Route path="/" exact>
+          <h1>Головна сторінка</h1>
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ frontend/src/pages/news/NewNews.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./NewNews.css";
+
+const NewNews = () => {
+  return <h1>Page New News!!!</h1>;
+};
+
+export default NewNews;
