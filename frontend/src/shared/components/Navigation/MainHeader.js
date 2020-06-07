16fdf4a45df07edf022ecf0aa6e75dd51e0cf25c
Ivan Babiy <ivanb07545@gmail.com>
2020-06-08 01:13:19 +0300

    some style changes

--- frontend/src/shared/components/Navigation/MainHeader.js
+++ frontend/src/shared/components/Navigation/MainHeader.js
@@ -3,7 +3,11 @@ import React from "react";
 import "./MainHeader.css";
 
 const MainHeader = (props) => {
-  return <header className="main-header">{props.children}</header>;
+  return (
+    <header>
+      <div className="main-header">{props.children}</div>
+    </header>
+  );
 };
 
 export default MainHeader;
