acb0b8325358b9b72239cf1e6882edca35e899fe
Ivan Babiy <ivanb07545@gmail.com>
2020-06-08 01:50:57 +0300

    add backend/first consig

---
+++ backend/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const newsRoutes = require("./routes/news-routes");
+
+const app = express();
+
+app.use(newsRoutes);
+
+app.listen(5000);
