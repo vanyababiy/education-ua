a2b246434581fee7334fa04f58fe44259a0dd435
Ivan Babiy <ivanb07545@gmail.com>
2020-06-08 02:34:46 +0300

    handling errors

--- backend/app.js
+++ backend/app.js
@@ -7,4 +7,13 @@ const app = express();
 
 app.use("/", newsRoutes);
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "Невідома помилка" });
+});
+
 app.listen(5000);
--- backend/routes/news-routes.js
+++ backend/routes/news-routes.js
@@ -13,7 +13,15 @@ const DUMMY_NEWS = [
 
 router.get("/news/:nId", (req, res, next) => {
   const NewsId = req.params.nId;
+
   const news = DUMMY_NEWS.find((n) => n.id === NewsId);
+
+  if (!news) {
+    const error = new Error("Неможливо знайти таку новину.");
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ news });
 });
 
