56e6971a9650188eef274fafc45652cdbf5ec3ae
Ivan Babiy <ivanb07545@gmail.com>
2020-06-08 11:22:08 +0300

    add models/http-error.js - obj for handling errors

--- backend/routes/news-routes.js
+++ backend/routes/news-routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const HttpError = require("../models/http-error");
+
 const router = express.Router();
 
 const DUMMY_NEWS = [
@@ -17,9 +19,7 @@ router.get("/news/:nId", (req, res, next) => {
   const news = DUMMY_NEWS.find((n) => n.id === NewsId);
 
   if (!news) {
-    const error = new Error("Неможливо знайти таку новину.");
-    error.code = 404;
-    throw error;
+    throw new HttpError("Неможливо знайти таку новину.", 404);
   }
 
   res.json({ news });
