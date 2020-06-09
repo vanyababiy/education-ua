fc11317e6edef587660ff1b11a1503c54c8c1c92
Ivan Babiy <ivanb07545@gmail.com>
2020-06-09 14:53:13 +0300

    to unique-validator/unique-validator done

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const Schema = mongoose.Schema;
 
@@ -8,5 +9,7 @@ const userSchema = new Schema({
   password: { type: String, required: true, minlength: 6 },
   news: { type: String, required: true },
 });
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model("User", userSchema);
